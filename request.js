eb49931ae49b95014abdf923490cd2609b085d7f
natac13 <sean.campbell13@gmail.com>
2015-09-09 09:57:40 -0400

    I find working with esling very frustrating and all the tutorial are lacking in detail

---
+++ request.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var request = require('request');
+var req = request.post('http://localhost:8099/');
+process.stdin.pipe(req).pipe(process.stdout);
--- transform.js
+++ transform.js
@@ -1,15 +1,16 @@
+'use strict';
 var through = require('through2');
 
-    function write (buffer, encoding, next) {
-        this.push(buffer.toString().toUpperCase());
-        next();
-    }
+function write (buffer, encoding, next) {
+    this.push(buffer.toString().toUpperCase());
+    next();
+}
 
 
 
-    function end (done) {
-        done();
-    }
+function end (done) {
+    done();
+}
 process.stdin.pipe(through(write, end)).pipe(process.stdout);
 
   // var through = require('through2');
